de4cbe22d26a16a9533bc3bfa5059baff6114c8b
Rodrigo <Rodrigobueno@iescastelar.com>
2023-09-24 14:10:58 +0200

    Revert "Añado Funcionalidad al proyecto java (suma)"
    This reverts commit d048a11928f0a15d3f3c8e71d5d03735cf435d77.

--- Programacion/HelloWorld.java
+++ Programacion/HelloWorld.java
@@ -1,18 +1,2 @@
-import java.util.Scanner;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo ,soy Rodrigo");
-        Scanner reader = new Scanner(System.in);
-        int numero1 = 0;
-        int numero2 = 0;
-        int resultado =0;
-        System.out.println("Introduce el primer número:");
-        numero1 = reader.nextInt();
-
-        System.out.println("Introduce el segundo número:");
-        numero2 = reader.nextInt();
-        resultado =numero1+numero2;
-        System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
-    }
+package PACKAGE_NAME;public class HelloWorld {
 }
